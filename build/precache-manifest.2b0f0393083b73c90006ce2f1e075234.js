5bca56b75ce3945f92490126ac98dea52a7ff25f
joonaba <42633391+joonaba@users.noreply.github.com>
2020-10-05 17:18:32 +0300

    frontin build lisätty

---
+++ build/precache-manifest.2b0f0393083b73c90006ce2f1e075234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39930bdb4b4db06803f0995a7b07697b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c5188beb588c3cd424c",
+    "url": "/static/css/main.06109991.chunk.css"
+  },
+  {
+    "revision": "d87433d4318c8d3ede1d",
+    "url": "/static/js/2.b32abe60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b32abe60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5188beb588c3cd424c",
+    "url": "/static/js/main.2c910be0.chunk.js"
+  },
+  {
+    "revision": "ca338480615863b14a0d",
+    "url": "/static/js/runtime-main.1d569e7e.js"
+  }
+]);
